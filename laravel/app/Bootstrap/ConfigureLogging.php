002c05a994dcd59615e7b8d7df05ccc5fd65e199
Fabian Torres <fa.iverson@gmail.com>
2015-12-03 17:58:47 -0300

    fix:  Class request does not exist

--- laravel/app/Bootstrap/ConfigureLogging.php
+++ laravel/app/Bootstrap/ConfigureLogging.php
@@ -5,16 +5,19 @@ namespace App\Bootstrap;
 use Illuminate\Log\Writer;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Bootstrap\ConfigureLogging as BaseConfigureLogging;
-use Request;
+use Illuminate\Http\Request;
+
 class ConfigureLogging extends BaseConfigureLogging {
 
 	protected function configureCustomHandler(Application $app, Writer $log)
 	{
-//		$filename = str_replace('/', '-', $app->request->path());
-//		$filename = str_replace('api-', '', $filename);
-//		$log->useDailyFiles(
-//			$app->storagePath().'/logs/'.$filename.'.log',
-//			$app->make('config')->get('app.log_max_files', 5)
-//		);
+		if(property_exists($app, 'request')) {
+			$filename = str_replace('/', '-', $app->request->path());
+			$filename = str_replace('api-', '', $filename);
+			$log->useDailyFiles(
+				$app->storagePath().'/logs/'.$filename.'.log',
+				$app->make('config')->get('app.log_max_files', 5)
+			);
+		}
 	}
 }
