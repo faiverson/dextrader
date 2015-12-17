0911dcfefcb4922b5382e04c50ad2c6657b48a92
Fabian Torres <fa.iverson@gmail.com>
2015-12-17 20:49:38 -0300

    migration comming soon table

---
+++ laravel/database/migrations/2015_12_17_234348_LeadsProductsCommingSoon.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LeadsProductsCommingSoon extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::create('leads_products_comming_soon', function (Blueprint $table) {
+			$table->bigIncrements('id');
+
+			$table->bigInteger('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users')
+				->onUpdate('cascade')->onDelete('cascade');
+
+			$table->smallInteger('product_id')->unsigned();
+			$table->foreign('product_id')->references('id')->on('products')
+				->onUpdate('cascade')->onDelete('cascade');
+			$table->timestamps();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+		Schema::drop('leads_products_comming_soon');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
