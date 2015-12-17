493096901772baf2f00e0c3ebcb5c49561fde016
Fabian Torres <fa.iverson@gmail.com>
2015-12-17 20:06:46 -0300

    access

--- laravel/config/ewallet.php
+++ laravel/config/ewallet.php
@@ -2,19 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Authentication Driver
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the authentication driver that will be utilized.
-    | This driver manages the retrieval and authentication of the users
-    | attempting to get access to protected areas of your application.
-    |
-    | Supported: "database", "eloquent"
-    |
-    */
-
     'merchant' => env('EWALLET_MERCHANT', 'Dex Trader'),
 	'guid' => env('EWALLET_GUID', 'ddd85e30-a29e-4baf-a84c-0ac4116e3405'),
 	'password' => env('EWALLET_PASSWORD', 'https://testewallet.com/eWalletWS/ws_Adapter.aspx'),
