d7935608dec89ff0d145d4db7eb92294871fbd4a
Luciano Seisdedos <lucho6d2@gmail.com>
2016-02-04 15:32:55 -0300

    marketing links changes

--- laravel/database/seeds/MarketingLinksSeeder.php
+++ laravel/database/seeds/MarketingLinksSeeder.php
@@ -21,7 +21,7 @@ class MarketingLinksSeeder extends Seeder
 			'title' => 'IB sales',
 			'campaign_id' => $fc->id,
 			'description' => 'IB sale page',
-			'image' => 'http://dextrader.com/ib',
+			'image' => '/front/assets/images/ib-sales.png',
 			'link' => 'http://dextrader.com/ib',
 			'products' => '1'
 		]);
@@ -30,46 +30,46 @@ class MarketingLinksSeeder extends Seeder
 			'title' => 'IB checkout',
 			'campaign_id' => $fc->id,
 			'description' => 'IB checkout page',
-			'image' => 'http://dextrader.com/ib',
-			'link' => 'http://secure.dextrader.com/ib',
+			'image' => '/front/assets/images/ib-checkout.png',
+			'link' => 'https://secure.dextrader.com/ib',
 			'products' => '1,2,3,4',
 		]);
 
-		MarketingLink::create([
-			'title' => 'IB PRO sales',
-			'campaign_id' => $fc->id,
-			'description' => 'IB PRO sale page',
-			'image' => 'http://dextrader.com/ib-pro',
-			'link' => 'http://dextrader.com/ib-pro',
-			'products' => '2'
-		]);
-
-		MarketingLink::create([
-			'title' => 'NA sales',
-			'campaign_id' => $fc->id,
-			'description' => 'IB PRO sale page',
-			'image' => 'http://dextrader.com/na',
-			'link' => 'http://dextrader.com/na',
-			'products' => '3'
-		]);
-
-		MarketingLink::create([
-			'title' => 'FX sales',
-			'campaign_id' => $fc->id,
-			'description' => 'FX sale page',
-			'image' => 'http://dextrader.com/fx',
-			'link' => 'http://dextrader.com/fx',
-			'products' => '4'
-		]);
-
-		MarketingLink::create([
-				'title' => 'ACADEMY sales',
-				'campaign_id' => $fc->id,
-				'description' => 'FX sale page',
-				'image' => 'http://dextrader.com/academy',
-				'link' => 'http://dextrader.com/academy',
-				'products' => '5'
-		]);
+//		MarketingLink::create([
+//			'title' => 'IB PRO sales',
+//			'campaign_id' => $fc->id,
+//			'description' => 'IB PRO sale page',
+//			'image' => 'http://dextrader.com/ib-pro',
+//			'link' => 'http://dextrader.com/ib-pro',
+//			'products' => '2'
+//		]);
+//
+//		MarketingLink::create([
+//			'title' => 'NA sales',
+//			'campaign_id' => $fc->id,
+//			'description' => 'IB PRO sale page',
+//			'image' => 'http://dextrader.com/na',
+//			'link' => 'http://dextrader.com/na',
+//			'products' => '3'
+//		]);
+//
+//		MarketingLink::create([
+//			'title' => 'FX sales',
+//			'campaign_id' => $fc->id,
+//			'description' => 'FX sale page',
+//			'image' => 'http://dextrader.com/fx',
+//			'link' => 'http://dextrader.com/fx',
+//			'products' => '4'
+//		]);
+//
+//		MarketingLink::create([
+//				'title' => 'ACADEMY sales',
+//				'campaign_id' => $fc->id,
+//				'description' => 'FX sale page',
+//				'image' => 'http://dextrader.com/academy',
+//				'link' => 'http://dextrader.com/academy',
+//				'products' => '5'
+//		]);
 
 		if (App::Environment() === 'local') {
 			$this->fetching();
--- laravel/database/seeds/SpecialOfferSeeder.php
+++ laravel/database/seeds/SpecialOfferSeeder.php
@@ -26,41 +26,41 @@ class SpecialOfferSeeder extends Seeder
 			'countdown' => (60 * 60 * 2) // 2 hs
 		]);
 
-		$mk = MarketingLink::where('link','http://dextrader.com/ib-pro')->first();
-		$prod = Product::where('name', 'PRO')->first();
-		SpecialOffer::create([
-			'funnel_id' => $mk->id,
-			'product_id' => $prod->id,
-			'amount' => 67,
-			'countdown' => (60 * 60 * 2) // 2 hs
-		]);
-
-		$mk = MarketingLink::where('link','http://dextrader.com/na')->first();
-		$prod = Product::where('name', 'NA')->first();
-		SpecialOffer::create([
-				'funnel_id' => $mk->id,
-				'product_id' => $prod->id,
-				'amount' => 67,
-				'countdown' => (60 * 60 * 2) // 2 hs
-		]);
-
-		$mk = MarketingLink::where('link','http://dextrader.com/fx')->first();
-		$prod = Product::where('name', 'FX')->first();
-		SpecialOffer::create([
-				'funnel_id' => $mk->id,
-				'product_id' => $prod->id,
-				'amount' => 67,
-				'countdown' => (60 * 60 * 2) // 2 hs
-		]);
-
-		$mk = MarketingLink::where('link','http://dextrader.com/academy')->first();
-		$prod = Product::where('name', 'ACADEMY')->first();
-		SpecialOffer::create([
-				'funnel_id' => $mk->id,
-				'product_id' => $prod->id,
-				'amount' => 67,
-				'countdown' => (60 * 60 * 2) // 2 hs
-		]);
+//		$mk = MarketingLink::where('link','http://dextrader.com/ib-pro')->first();
+//		$prod = Product::where('name', 'PRO')->first();
+//		SpecialOffer::create([
+//			'funnel_id' => $mk->id,
+//			'product_id' => $prod->id,
+//			'amount' => 67,
+//			'countdown' => (60 * 60 * 2) // 2 hs
+//		]);
+//
+//		$mk = MarketingLink::where('link','http://dextrader.com/na')->first();
+//		$prod = Product::where('name', 'NA')->first();
+//		SpecialOffer::create([
+//				'funnel_id' => $mk->id,
+//				'product_id' => $prod->id,
+//				'amount' => 67,
+//				'countdown' => (60 * 60 * 2) // 2 hs
+//		]);
+//
+//		$mk = MarketingLink::where('link','http://dextrader.com/fx')->first();
+//		$prod = Product::where('name', 'FX')->first();
+//		SpecialOffer::create([
+//				'funnel_id' => $mk->id,
+//				'product_id' => $prod->id,
+//				'amount' => 67,
+//				'countdown' => (60 * 60 * 2) // 2 hs
+//		]);
+//
+//		$mk = MarketingLink::where('link','http://dextrader.com/academy')->first();
+//		$prod = Product::where('name', 'ACADEMY')->first();
+//		SpecialOffer::create([
+//				'funnel_id' => $mk->id,
+//				'product_id' => $prod->id,
+//				'amount' => 67,
+//				'countdown' => (60 * 60 * 2) // 2 hs
+//		]);
 
 		Training::reguard();
 		$this->command->info("Specials offers finished");
