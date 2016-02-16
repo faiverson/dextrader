8a83e289ae2da1dde0744f2f4e7ff193c6d4b5e0
Fabian Torres <fa.iverson@gmail.com>
2016-02-16 15:16:28 -0300

    users

--- admin/src/modules/users/users.js
+++ admin/src/modules/users/users.js
@@ -65,6 +65,11 @@ angular.module('app.home', ['ui.router', 'ui.bootstrap.showErrors', 'datatables'
 				last_name: null,
 				email: null,
 				apply: function () {
+					this.toApply.from = null;
+					this.toApply.to = null;
+					this.toApply.first_name = null;
+					this.toApply.last_name = null;
+					this.toApply.email = null;
 
 					if (angular.isDefined(this.from) && this.from !== null) {
 						this.toApply.from = moment(this.from).format('YYYY-MM-DD');
@@ -92,9 +97,7 @@ angular.module('app.home', ['ui.router', 'ui.bootstrap.showErrors', 'datatables'
 			};
 
 			$scope.$watch('filters.from', function (nv, ov) {
-				if (angular.isDefined(nv) && nv !== ov) {
-					$scope.filters.apply();
-				}
+				$scope.filters.apply();
 			});
 
 			$scope.$watch('filters.to', function (nv, ov) {
