84677e0926a22595b96430aa47dafe69ee1b5df7
Fabian Torres <fa.iverson@gmail.com>
2016-03-08 20:44:44 -0300

    fix offers

--- laravel/app/Gateways/SpecialOfferGateway.php
+++ laravel/app/Gateways/SpecialOfferGateway.php
@@ -14,18 +14,30 @@ class SpecialOfferGateway extends AbstractGateway {
 
 	protected $updateValidator;
 
+	protected $productGateway;
+
 	protected $errors;
 
-	public function __construct(SpecialOfferRepository $repository, SpecialOfferCreateValidator $createValidator, SpecialOfferUpdateValidator $updateValidator)
+	public function __construct(SpecialOfferRepository $repository, SpecialOfferCreateValidator $createValidator, SpecialOfferUpdateValidator $updateValidator, ProductGateway $productGateway)
 	{
 		$this->repository = $repository;
 		$this->createValidator = $createValidator;
 		$this->updateValidator = $updateValidator;
+		$this->product = $productGateway;
 	}
 
 	public function findByFunnel($page)
 	{
-		return $this->repository->findByFunnel($page);
+		$offers = $this->repository->findByFunnel($page);
+		if($offers->count() > 0) {
+			$ids = array_unique(array_column($offers->toArray(), 'product_id'));
+			$products= $this->product->findIn($ids);
+		}
+
+		return [
+			'products' => $products,
+			'offers' => $offers
+		];
 	}
 
 	public function findIn($ids)
